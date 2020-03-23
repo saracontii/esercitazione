8fe891615b3e58f1fce588ad9e243ca320b91e1f
Sara Conti <saraconti0207@gmail.com>
2020-03-23 10:55:18 +0100

    added feature

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ if x % 2 == 0:
     print("il numero che hai inserito è pari")
 else:
     ("il numero che hai inserito è dispari")
+print(x*2, " questo è il doppio del numero che hai inserito")
+print(x/2, " questa è la metà del numero che hai inserito")
+f = 1
+for e in range(1, x+1):
+    f*= e
+print("{} è il fattoriale di {}".format(f, x))
